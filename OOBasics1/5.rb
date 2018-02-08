48183d36d970ec77530ec5f171e4a348eca5689a
nathias <nihilnegativum@protonmail.com>
2018-02-08 16:19:10 +0100

    Easy1 exercises complete.

---
+++ OOBasics1/5.rb
@@ -0,0 +1,9 @@
+class Cat
+  def initialize(name)
+    @name = name
+    puts "I'm #{name}."
+  end
+end
+
+kitty = Cat.new('Sophie')
+
---
+++ OOBasics1/6.rb
@@ -0,0 +1,11 @@
+class Cat
+  def initialize(name)
+    @name = name
+  end
+  def greet
+    puts "Hello! My name is #{@name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
