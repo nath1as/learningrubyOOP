f78ec6077795998903b474d7fdabf9997383b38d
nathias <nihilnegativum@protonmail.com>
2018-02-09 19:01:58 +0100

    OOOBasics 3 done.

---
+++ OOBasics3/2.rb
@@ -0,0 +1,20 @@
+class Vehicle
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+end
+
+class Truck < Vehicle
+  def initialize
+    super
+    start_engine
+  end
+  def start_engine
+    puts 'Ready to go!'
+  end
+end
+
+truck1 = Truck.new(1994)
+puts truck1.year
